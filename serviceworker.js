2bca036e9e3769fb6d52e905fe97e7214533c56c
setana.jp <setana.jp@gmail.com>
2020-07-29 17:27:18 +0900

    R01

--- serviceworker.js
+++ serviceworker.js
@@ -1,2 +1,23 @@
-self.addEventListener('fetch', function(){});
+var cacheName = 'PWA Gyrosensor';
+var filesToCache = [
+  'index.html',
+];
+
+/* サービスワーカー起動して、コンテンツをキャッシュする */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* オフライン時はキャッシュからコンテンツを取得する */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
 
